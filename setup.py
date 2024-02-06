c0c36ccc35c7df4154940ab0549c6af682789aa6
dwillco2 <dwillco2@ed.ac.uk>
2024-02-06 13:30:15 +0000

    added setup moved files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gen_gaussian',
+      version='1.0',
+      description='Generating Gaussian16 inputs',
+      author='Dominic Willcox',
+      author_email='dwillco2@ed.ac.uk',
+      py_modules=['gen_gaussian'],
+     )
